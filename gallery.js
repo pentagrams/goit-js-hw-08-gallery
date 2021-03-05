01f1550c30f301fb47ed30c64a155496325cb845
Max M <maxminullin@gmail.com>
2021-03-05 20:51:13 +0200

    end

--- gallery.js
+++ gallery.js
@@ -2,20 +2,55 @@ class gallerySlider {
 
 
       constructor(selector, items = []) {
-            this.element = document.querySelector(selector)
             this.items = items;
+            this.element = document.querySelector(selector);
+            // this.containerWidth = this.element.clientWidth;
+            // this.currentIndex = 0;
+            
       }
 
       createMarkup({ preview, original, description }) {
-            return `<li>
-                        <img src="${preview}" data-original="${original}" alt="${description}" />
+            return `<li class="gallery__item">
+                        <a
+                              class="gallery__link"
+                              href="${original}">
+                              <img
+                                    class="gallery__image"
+                                    src="${preview}"
+                                    data-source="${original}"
+                                    alt="${description}"
+                                    />
+                        </a>         
                   </li>`;
       }
 
+
       createlist() {
-            const sliderList = this.items.map(this.createMarkup).join('');
-            return `<div class="slider__inner">${sliderList}</div>`;
+            return this.items.map(this.createMarkup).join('');
       }
+
+
+      // moveNext() {
+      //       if (this.currentIndex === this.items.length - 1) {
+      //             return
+      //       }
+      //       this.currentIndex += 1;
+      // }
+
+
+      // movePrev() {
+      //      if (this.currentIndex === 0) {
+      //             return
+      //       }
+      //       this.currentIndex -= 1; 
+      // }
+
+
+      // moveTo() {
+            
+      // }
+
+      
 
       render() {
             const temlate = this.createlist();
--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 import gallery from './gallery-items.js';
 import gallerySlider from './gallery.js'
 
+
 const listGalleryRef = document.querySelector('.js-gallery');
-const backdropRef = document.querySelector('.js-lightbox');
+const buttonCloseRef = document.querySelector('button[data-action="close-lightbox"]');
+const modalRef = document.querySelector(".js-lightbox");
+const fullSizeImgRef = document.querySelector(".lightbox__image");
+
+
+
+
+listGalleryRef.addEventListener("click", clickOnImages);
+modalRef.addEventListener("click", clickOnModal);
+
+
 
 const slider = new gallerySlider('.js-gallery', gallery)
 slider.render();
+
+
+
+
+function clickOnImages(event) {
+      event.preventDefault()
+      if (event.target === event.currentTarget) {
+            return
+      }
+      modalRef.classList.add("is-open");
+      fullSizeImgRef.setAttribute("src", event.target.dataset.source);
+}
+
+
+function clickOnModal(event) {
+  if (event.target.nodeName === buttonCloseRef.nodeName) {
+    modalRef.classList.remove("is-open");
+    listGalleryRef.removeEventListener("click", clickOnModal);
+  }
+}
+
+
+
 
